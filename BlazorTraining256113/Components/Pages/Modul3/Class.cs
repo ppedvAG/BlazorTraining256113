08afbb33ba5a84cc413d0fbfd6445db59a6e6e81
Hannes Preishuber <hannesp@ppedv.de>
2026-02-26 13:50:46 +0100

    property

---
+++ BlazorTraining256113/Components/Pages/Modul3/Class.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorTraining256113.Components.Pages.Modul3
+{
+    public class Class
+    {
+
+        public int MyProperty { get; set; }
+		
+		
+		private int myVar;
+
+		public int MyProperty
+		{
+			get { return myVar; }
+			set { myVar = value; }
+		}
+
+	}
+}
